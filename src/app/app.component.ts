acc1aaadb53e8541baea908ec3bfb552201795a0
KarineAlsa <alsa.karine@gmail.com>
2024-10-17 13:47:58 -0600

    16-v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'captcha-16';
+  key = environment.key
+  captchaToken: string | null = null;
+
+  onCaptchaResolved(token: string) {
+    console.log("reCAPTCHA v2 Token:", token);
+    this.captchaToken = token;
+  }
+
+  submit() {
+    if (this.captchaToken) {
+      console.log("Enviar al backend con token:", this.captchaToken);
+    } else {
+      console.log("Completa el captcha antes de enviar.");
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecaptchaModule } from 'ng-recaptcha';
 
 @NgModule({
-  declarations: [
+  declarations: [	
     AppComponent
-  ],
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RecaptchaModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
